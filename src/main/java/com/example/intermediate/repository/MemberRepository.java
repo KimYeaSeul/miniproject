3f0fac1c44158b336b5cfefb435223eecdd9e4f0
Rzreo <zuzu5313@naver.com>
2022-08-15 19:50:24 +0900

    구글로그인 추가 + 에러메세지 영문으로 출력

--- src/main/java/com/example/intermediate/repository/MemberRepository.java
+++ src/main/java/com/example/intermediate/repository/MemberRepository.java
@@ -12,4 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   Optional<Member> findByUsername(String username);
 
   Optional<Member> findByKakaoId(Long kakaoId);
+
+  Optional<Member> findByGoogleId(String googleId);
 }
