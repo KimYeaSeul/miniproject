3b6471726189cb1caa27be0de56f238928f3b03f
Rzreo <zuzu5313@naver.com>
2022-08-17 11:44:41 +0900

    마이 페이지

--- src/main/java/com/example/intermediate/repository/PostRepository.java
+++ src/main/java/com/example/intermediate/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.intermediate.repository;
 
+import com.example.intermediate.domain.Member;
 import com.example.intermediate.domain.Post;
 
 import java.util.List;
@@ -8,9 +9,13 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+
+    List<Post> findAllByMemberOrderByModifiedAtDesc(Member member);
 
     @Override
     Page<Post> findAll(Pageable pageable);
